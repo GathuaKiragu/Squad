4e323ede0bc786aee202b69830c1dd683399314f
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-01 21:08:10 +0300

    All squad test passes correctly

--- src/test/SquadTest.java
+++ src/test/SquadTest.java
@@ -9,5 +9,41 @@ public class SquadTest {
     Squad firstSquad = new Squad("Moringa Gung","Bringing Technology to Life");
     assertTrue(firstSquad instanceof Squad);
   }
-  
+// Test to get the size of the squad
+  @Test
+    public void Squad_getSize_int(){
+    Squad firstSquad = new Squad(4);
+    assertEquals(4, firstSquad.getSize());
+  }
+// Test to check if we can get the name form the user
+  @Test
+public void Squad_getName_String(){
+  Squad firstSquad = new Squad("Moringa Team");
+  assertEquals("Moringa Team", firstSquad.getName());
+  }
+// Test to check if we can get the Squads passion
+  @Test
+  public void Squad_getPassion_String(){
+    Squad firstSquad = new Squad("Moringa Team");
+    assertEquals("raising awareness about windowWashing", firstSquad.getPassion());
+  }
+
+  @Test
+  public void Squad_instancesListClearsCorrectly_0(){
+    Squad.clear();
+    assertEquals(0, Squad.all().size());
+  }
+
+  @Test
+  public void Squad_allReturnsAll_true() {
+    assertTrue(Squad.all().contains(firstSquad));
+  }
+
+  @Test
+  public void Squad_findReturnsById_Squad() {
+    assertEquals(Squad.find(firstSquad.getId()), firstSquad);
+  }
+
+}
+
 }
