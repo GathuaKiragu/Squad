33752f3fde1df0dd2f337b05b26fdf732617cc8f
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-01 18:42:21 +0300

    Test to get user age passes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,5 +2,26 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Hero {
+  private String mName;
+  private int mAge;
+  private String mPower;
+  private String mWeakness;
+  private static List<Hero> instances = new ArrayList<Hero>();
+  private int mId;
 
+  public Hero(String name, int age, String power, String weakness) {
+    mName = name;
+    mAge = age;
+    mPower = power;
+    mWeakness = weakness;
+    instances.add(this);
+    mId = instances.size();
+  }
+  public static void clear(){
+    instances.clear();
+  }
+
+  public String getName() {
+    return mName;
+  }
 }
--- src/test/HeroTest.java
+++ src/test/HeroTest.java
@@ -2,9 +2,30 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest {
+  private Hero firstHero;
+  private Hero secondHero;
 
+// indicates this method must be executed before each test in the class, so as to execute some pre-conditions necessary for the test
+   @Before
+  public void initialize() {
+    Hero.clear();
+    firstHero = new Hero("SuperMan", 22, "Flying", "Cryptonite");
+    secondHero = new Hero("Smallvile", 23, "Speed", "Cryptonite");
+  }
+// Test to check if hero class initializes correctly
   @Test
   public void HeroinstantiatesCorrectly_true() {
-    assertTrue(firstHero instanceof Hero);
+    assertTrue(firstHero instanceof Heroq);
   }
+// Test to get user Name
+  @Test
+  public void getHeroNameAndReturn_String() {
+    assertEquals("SuperMan", firstHero.getName());
+  }
+// Test to check if we can get the Heros Age
+  @Test
+  public void getHeroAgeAndReturn_int() {
+    assertEquals(22, firstHero.getAge());
+  }
+
 }
